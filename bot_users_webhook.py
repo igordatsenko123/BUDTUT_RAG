36a94ee6c84f4b642214cd63cd70a8600d6626b4
Igor Datsenko <Igor.Datsenko@rzm.com.ua>
2025-11-17 11:13:56 +0200

    new site

--- bot_users_webhook.py
+++ bot_users_webhook.py
@@ -57,7 +57,7 @@ NAME, SURNAME, PHONE, SPECIALTY, EXPERIENCE = range(5)
 
 menu_keyboard = ReplyKeyboardMarkup(
     [
-        [KeyboardButton("💪 Навчальний курс", web_app=WebAppInfo(url="https://tally.so/r/w4RNRo"))]
+        [KeyboardButton("💪 Навчальний курс", web_app=WebAppInfo(url="https://safe-weld-path.lovable.app/module"))]
     ],
     resize_keyboard=True
 )
